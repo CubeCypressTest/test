ec70cb72a851b20dd237db689bbfceff85265a94
cube.cypress.test <cube.cypress.test@gmail.com>
2023-03-01 13:22:28 +0000

    test

--- schema/Orders.js
+++ schema/Orders.js
@@ -1,5 +1,5 @@
 cube(`Orders`, {
-    sql: `
+  sql: `
     select 1 as id, 100 as amount, 'new' status
     UNION ALL
     select 2 as id, 200 as amount, 'new' status
@@ -10,30 +10,29 @@ cube(`Orders`, {
     UNION ALL
     select 5 as id, 600 as amount, 'shipped' status
     `,
-  
-    preAggregations: {
-      // Pre-Aggregations definitions go here
-      // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+    main: {
+      measures: [Orders.count]
     },
-  
-    measures: {
-      count: {
-        type: `count`
-      },
-  
-      totalAmount: {
-        sql: `amount`,
-        type: `sum`
-      }
-    },
-  
-    dimensions: {
-      status: {
-        sql: `status`,
-        type: `string`
-      }
+    main11: {
+      measures: [Orders.count]
     }
-  });
-  
-
-
+  },
+  measures: {
+    count: {
+      type: `count`
+    },
+    totalAmount: {
+      sql: `amount`,
+      type: `sum`
+    }
+  },
+  dimensions: {
+    status: {
+      sql: `status`,
+      type: `string`
+    }
+  }
+});
